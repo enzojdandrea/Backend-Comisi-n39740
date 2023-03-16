9c6b0facf06881bbcd510eb215f7361ce6b78195
Enzo D'Andrea <enzojdandrea@gmail.com>
2023-03-16 20:14:31 -0300

    agregado try catch

--- Clase05-productManager-Archivos.js
+++ Clase05-productManager-Archivos.js
@@ -8,12 +8,18 @@ class productsManage1 {
     }
 
     async loadProdcuts() {
-        const products = await fs.promises.readFile(this.path, 'utf-8')
-        if(products.length!=0){
-            this.products = JSON.parse(products)
-            const lengt=this.products.length
-            this.id=this.products[lengt-1].id
+        try {
+            const products = await fs.promises.readFile(this.path, 'utf-8')
+            if (products.length != 0) {
+                this.products = JSON.parse(products)
+                const lengt = this.products.length
+                this.id = this.products[lengt - 1].id
+            }
+        } catch (error) {
+            const products = await fs.promises.writeFile(this.path, JSON.stringify(this.products))
+            console.log(error)
         }
+
     }
 
     async addproduct(product) {
@@ -21,7 +27,7 @@ class productsManage1 {
             if (this.products.length != 0) {
                 const { code } = product
                 const productFind = this.products.find((product) => product.code === code);
-                this.id=this.id+1
+                this.id = this.id + 1
                 if (productFind) {
                     throw Error("Ya exisiste un producto con ese codigo")
                 }
@@ -29,7 +35,7 @@ class productsManage1 {
             this.products.push(
                 {
                     ...product,
-                    id:this.id
+                    id: this.id
                 })
             await fs.promises.writeFile(this.path, JSON.stringify(this.products))
             return 'Producto agregado'
@@ -47,7 +53,7 @@ class productsManage1 {
     async getProductsById(id) {
         const products = await fs.promises.readFile(this.path, 'utf-8')
         const productsParse = JSON.parse(products)
-    
+
         const product = productsParse.find((prod) => prod.id === id);
 
         if (!product) {
@@ -56,25 +62,25 @@ class productsManage1 {
         return product;
     }
 
-    async updateProduct(id,atribute,newData){
+    async updateProduct(id, atribute, newData) {
         const products = await fs.promises.readFile(this.path, 'utf-8')
         const productsParse = JSON.parse(products)
 
-        const foundProduct = productsParse.find((prod)=>{
-            return prod.id===id
+        const foundProduct = productsParse.find((prod) => {
+            return prod.id === id
         })
         if (!foundProduct) {
             throw Error(`No existe ningun porducto con ID: ${id}`);
         }
-        foundProduct[atribute]=newData;
+        foundProduct[atribute] = newData;
 
         await fs.promises.writeFile(this.path, JSON.stringify(productsParse))
     }
 
-    async deleteProduct(id){
+    async deleteProduct(id) {
         const products = await fs.promises.readFile(this.path, 'utf-8')
         const productsParse = JSON.parse(products)
-        const newProducts = productsParse.filter((produ)=>produ.id!=id)
+        const newProducts = productsParse.filter((produ) => produ.id != id)
         await fs.promises.writeFile(this.path, JSON.stringify(newProducts))
     }
 }
@@ -89,7 +95,7 @@ const main = async () => {
         priece: 40,
         thumbnail: "Sin Imagen",
         stock: 100,
-        code: "abc127"
+        code: "abc123"
     })
 
     console.log("Todos los Prodcutos")
@@ -100,13 +106,13 @@ const main = async () => {
     const productByID = await productsManager2.getProductsById(1)
     console.log(productByID)
 
-    await productsManager2.updateProduct(1,"title","Cambiado")
+    await productsManager2.updateProduct(1, "description", "Cambiado")
     console.log("Product Cambiado")
     const productCambiado = await productsManager2.getProductsById(1)
     console.log(productCambiado)
 
     await productsManager2.deleteProduct(2)
-    console.log("Todos los Prodcutos menos ID 4")
+    console.log("Todos los Prodcutos menos ID 2")
     const productsDelete = await productsManager2.getProducts()
     console.log(productsDelete)
 }
